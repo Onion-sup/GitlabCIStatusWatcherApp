ab91b3a34b3d3db505c3ab34377d37998b4688e8
Onion-sup <simeon.jour@gmail.com>
2021-12-11 18:00:44 +0100

    Api calls from project to branch to pipeline status

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import { GITLAB_API_TOKEN, GITLAB_HOST } from './secrets'
 
-function getProjectArray(){
+function getGitlabProjects(){
   const url = "https://" + GITLAB_HOST + "/api/v4/projects?simple=true&private_token=" + GITLAB_API_TOKEN
   return (
     fetch(url)
@@ -35,7 +35,26 @@ function getProjectArray(){
     .catch((error) => console.error(error))
     )
 }
-console.log(getProjectArray().then((json) => json))
+function getProjectBranches(idProject){
+  const url = "https://" + GITLAB_HOST + "/api/v4/projects/" + idProject + "/repository/branches?private_token=" + GITLAB_API_TOKEN
+  return (
+    fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+    )
+}
+
+function getPipelineFromCommit(idProject, commitSha){
+  const url = "https://" + GITLAB_HOST + "/api/v4/projects/" + idProject + "/pipelines?sha="+ commitSha + "&private_token=" + GITLAB_API_TOKEN
+  return (
+    fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+    )
+}
+getGitlabProjects().then((projects) => console.log("project", projects[0].id, projects[0].name))
+getProjectBranches(34).then((branches) => console.log("branch", branches[1].commit.id))
+getPipelineFromCommit(34, branches[1].commit.id).then((pipeline) => console.log("pipeline", pipeline[0].status))
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
