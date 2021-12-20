7635641ec2182e103a244477c56d1b57f5d5fe62
Onion-sup <simeon.jour@gmail.com>
2021-12-20 12:34:38 +0100

    add branch status pellet

--- components/MainPanel.js
+++ components/MainPanel.js
@@ -52,8 +52,8 @@ export class MainPannel extends React.Component {
                     flatListProps={{
                         keyExtractor: (_, idx) => idx,
                         renderItem: ({ item }) =>
-                            <TouchableOpacity onPress={() => this.setState( { projectSelected: item, projectsFound: [] }, () => this.updateProjectBranches())}>
-                                <Text>{item.name}</Text>
+                            <TouchableOpacity style={styles.suggestionListItem} onPress={() => this.setState( { projectSelected: item, projectsFound: [] }, () => this.updateProjectBranches())}>
+                                <Text style={styles.text}>{item.name}</Text>
                             </TouchableOpacity>
                     }}
                 />
@@ -66,70 +66,128 @@ export class MainPannel extends React.Component {
             <View style={styles.branchListContainer}>
                 <FlatList
                 data={this.state.branches}
-                renderItem= {({ item }) => 
-                    <TouchableOpacity onPress={() => this.setState( { branchSelected: item }, () => this.updatePipeline())}>
-                        <Text>{item.name}</Text>
-                    </TouchableOpacity>
-                }
+                renderItem= {({ item }) => this.renderBranchItem(item) }
                 keyExtractor={item => item.name}
+                extraData={this.state.pipeline}
             /> 
           </View>
+        )
+    }
+    renderBranchItem(branch){
+        return (
+            <TouchableOpacity style={styles.branchListItem} onPress={() => this.setState( { branchSelected: branch }, () => this.updatePipeline())}>
+                <Text style={{fontSize: 20, flex:0.9}} numberOfLines={1}>{branch.name}</Text>
+                { (this.state.pipeline && this.state.branchSelected.name === branch.name) ?  this.renderStatusPellet() : null }
+            </TouchableOpacity>
+        )
+    }
+    renderStatusPellet(){
+        return(
+            <View style={styles.circle}></View>
         )
     }
     renderPipeline(){
         return(
             <View style={styles.pipelineContainer}>
                 <Text>PipelineStatus: { (this.state.pipeline) ? this.state.pipeline.status : "unknown" }</Text>
+                <FlatList
+                    data={this.state.pipelineJobs}
+                    renderItem= {({ item }) => 
+                        {this.renderJobStatus(item)}
+                    }
+                    keyExtractor={item => item.name}
+                />
             </View>
         )
     }
-    renderJobsStatus(){
+    renderJobStatus(job){
         return(
-            <FlatList
-            data={this.state.branches}
-            renderItem= {({ item }) => 
-                <Text>{item.name}: {item.status}</Text>
-            }
-            keyExtractor={item => item.name}
-          /> 
+            <View style={styles.jobContainer}>
+                <Text style={styles.jobName}>
+                    {job.name}
+                </Text>
+                <View style={styles.jobStatus}>
+
+                </View>
+            </View>
         )
     }
     render() {
         return (
-            <View>
+            <View style={styles.mainContainer}>
                 { this.renderProjectSearchBar() }
-                { this.renderBranchList() }
-                { this.renderPipeline() }
-                {/* { this.renderJobsStatus() } */}
+                <View style={styles.branchListAndPipelineContainer}>
+                    { this.renderBranchList() }
+                    { this.renderPipeline() }
+                </View>
             </View>
             )
         }
 }
-
+const colors = {
+    background: "#554488",
+    displayZones: "#D0D3D4",
+    listItem: "#ECF0F1",
+    success: "#42ba96"
+}
 const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: colors.background,
+        fontFamily: 'san'
+    },
+    text:{
+        fontSize: 20
+    },
+    suggestionListItem: {
+        backgroundColor: colors.listItem
+    },
+    branchListItem: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        borderBottomWidth: 1
+    },
     autocompleteContainer: {
-      left: 0,
-      position: 'absolute',
-      right: 0,
-      top: 0,
+        margin: 10,
+        flex: 1,
+        left: 0,
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        zIndex: 1
     },
     searchBar: {
-        borderWidth: 1,
-        borderRadius: 20
+        backgroundColor: colors.displayZones,
+        fontSize: 20
     },
-    pipelineStatusContainer: {
-        top: 100
-    },
-    pipelineContainer: {
-        borderWidth: 1,
-        borderRadius: 20
+    branchListAndPipelineContainer: {
+        top: 60,
+        flex: 0.9,
+        justifyContent: "space-between",
     },
     branchListContainer: {
-        top: 100,
-        height: 100,
+        backgroundColor: colors.displayZones,
+        flex: 0.3,
         paddingLeft: 10,
         paddingRight: 10,
-        borderWidth: 1,
-        borderRadius: 20
-        }
+    },
+    pipelineContainer: {
+        backgroundColor: colors.displayZones,
+        flex: 0.68,
+        Width: 1,
+    },
+    jobContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        borderWidth: 1
+    },
+    circle: {
+        width: 25,
+        height: 25,
+        borderRadius: 25/2,
+        backgroundColor: colors.success,
+        borderWidth: 1
+    }
   });
