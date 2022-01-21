e10240b8711caae53ac7d0d45f34735714c9f8cf
Onion-sup <simeon.jour@gmail.com>
2022-01-21 20:27:57 +0100

    add awsome logo

--- styles.js
+++ styles.js
@@ -2,10 +2,10 @@ export const colors = {
     background: "#554488",
     displayZones: "#D0D3D4",
     listItem: "#ECF0F1",
-    pending: "#F1C40F",
-    running: "#3498DB",
-    success: "#42ba96",
-    failed: "#E74C3C",
+    pending: "#FC9403",
+    running: "#1F78D1",
+    success: "#1AAA55",
+    failed: "#DB3B21",
     canceled: "#F0F3F4"
 }
 
